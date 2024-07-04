9764c22ad836958ae8400272d274eb8943a53069
15625573716 <158493504+15625573716@users.noreply.github.com>
2024-07-04 11:21:30 +0800

    Update APP.py

--- APP.py
+++ APP.py
@@ -39,4 +39,4 @@ if right_column.button("Predict"):  # 在右侧列放置预测按钮
     Predict_proba = model.predict_proba(X)[:, 1][0]
     Predict_proba1=1-Predict_proba
     # 输出预测结果到空白容器
-    result_container.subheader(f"Probability of predicting unfavourable discharge outcome: {'%.2f' % (Predict_proba * 100)}%")
+    result_container.subheader(f"Probability of predicting unfavourable discharge outcome: {'%.2f' % (Predict_proba1 * 100)}%")
